2e23de3e71568058a2aa180b6630307981f3c42f
Alberto Garza Ayala <103970436+Alberto9204@users.noreply.github.com>
2022-12-20 01:26:35 -0600

    Add files via upload

---
+++ stored_procedure_insert_data.sql
@@ -0,0 +1,11 @@
+CREATE PROCEDURE `insert_new_client`(
+IN nombre_nuevo_cliente VARCHAR(50),
+IN plato_ordenado_nuevo_cliente VARCHAR(30), 
+IN correo_nuevo_cliente VARCHAR(50))
+BEGIN
+INSERT INTO nuevo_cliente(nombre_nuevo_cliente, plato_ordenado_nuevo_cliente, correo_nuevo_cliente) 
+VALUES ( 
+nombre_nuevo_cliente,
+plato_ordenado_nuevo_cliente,
+correo_nuevo_cliente);
+END
